01fa737396a31416196909954ebc2ae94750e5c2
Daniel Walker <brotlov_1@hotmail.com>
2018-04-29 13:59:19 +1000

    Basic UI Layout + Side Swipeable navigation with Date changing text

---
+++ Wicket/Wicket/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Wicket
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			//MainPage = new Wicket.MainPage();
+            MainPage = new Master();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Wicket/Wicket/MasterMaster.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wicket
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public MasterMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new MasterMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MasterMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MasterMenuItem> MenuItems { get; set; }
+            
+            public MasterMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<MasterMenuItem>(new[]
+                {
+                    new MasterMenuItem { Id = 0, Title = "Games", TargetType = typeof(Games) },
+                    new MasterMenuItem { Id = 0, Title = "Settings", TargetType = typeof(Settings) },
+                    new MasterMenuItem { Id = 1, Title = "Donate", TargetType = typeof(Donate) },
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
